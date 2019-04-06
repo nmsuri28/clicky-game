0b6ffb231751080a25e70308c69ec8809c3c7e60
Natosha <nmsuri28@gmail.com>
2019-04-06 17:00:39 -0500

    Updates

---
+++ precache-manifest.24790a4d6dabd705043363bbca81668c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53014e4f9e1c7a94c145d96a2c1da71c",
+    "url": "/clicky-game/static/media/marvel-logo-card-1560x876_2.53014e4f.jpg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "015b039891e799ae939f",
+    "url": "/clicky-game/static/js/main.fdd72972.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "015b039891e799ae939f",
+    "url": "/clicky-game/static/css/main.4c26bfef.chunk.css"
+  },
+  {
+    "revision": "4e33a4bb6b953e1d39d5e2102e8d0da2",
+    "url": "/clicky-game/index.html"
+  }
+];
